c9c626c5db229b3a555fda899629e6f133263386
Kevin S. Chen <kschen@tigressdata2.princeton.edu>
2024-02-20 11:35:05 -0500

    fix parpool

--- bash_ssm_par.sh
+++ bash_ssm_par.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=matlab        # create a short name for your job
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks=8               # total number of tasks across all nodes
+#SBATCH --ntasks=4               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=48:01:00          # total run time limit (HH:MM:SS)
